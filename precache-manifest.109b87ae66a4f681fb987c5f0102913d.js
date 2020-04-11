8b4b8e63fc86a3daef91e7618059bcb096dc28fa
rajeev ranjan <talegarry1202@gmail.com>
2020-04-11 12:34:57 +0530

    Updates

---
+++ precache-manifest.109b87ae66a4f681fb987c5f0102913d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ec448b6cd683432875562380dd98d5",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "fe529ed216c9f4a0123e",
+    "url": "/Monster-Rolodex/static/css/main.91e182d2.chunk.css"
+  },
+  {
+    "revision": "f3bc58ad93c95ac2dfd4",
+    "url": "/Monster-Rolodex/static/js/2.7158d2bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Rolodex/static/js/2.7158d2bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe529ed216c9f4a0123e",
+    "url": "/Monster-Rolodex/static/js/main.d3763663.chunk.js"
+  },
+  {
+    "revision": "8f653a392ca2f2785e81",
+    "url": "/Monster-Rolodex/static/js/runtime-main.266800ad.js"
+  }
+]);
